996a93e66fedfebb6fcb3ab1931458e925589bc7
Hay Kranen <huskyr@gmail.com>
2012-03-29 13:07:46 +0200

    finished up the mutate event

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
+var undef;
+
 module("set");
 
 test("change events", function() {
-    expect(6);
+    expect(8);
 
     var module = Stapes.create();
 
@@ -12,9 +14,27 @@ test("change events", function() {
             ok(key === 'name' || key === 'instrument', 'change event when name is set');
         },
 
-        'change:name' : function(value, oldValue) {
+        'change:name' : function(value) {
             equal(value, 'Emmylou', 'name attribute changed');
-            equal(oldValue, 'Johnny', 'old value of name attribute');
+        },
+
+        'mutate' : function(value) {
+            ok(
+                value.key !== undef && value.newValue !== undef && value.oldValue !== undef,
+                "mutate event throws a lot of extra info"
+            );
+        },
+
+        'mutate:name' : function(value) {
+            deepEqual(
+                value,
+                {
+                    key : "name",
+                    newValue : "Emmylou",
+                    oldValue : "Johnny"
+                },
+                "mutate namespaed event throws a lot of extra info"
+            );
         },
 
         'create' : function(key) {
