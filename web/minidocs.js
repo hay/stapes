29d2da80db84430ef397138514d0a25156ac6da8
Josh W <josher19@users.sf.net>
2012-11-30 17:21:34 +0800

    With minidocs

---
+++ web/minidocs.js
@@ -0,0 +1,95 @@
+(function() {
+    /**
+     * Mini documentation using titles on code spans created with rainbowco.de or similiar. 
+     * Args: docs object {functionName:documentation_string,...}, optional domElements collection or selector string.
+     * Uses: document.querySelectorAll defined in HTML5. Need a shim for obsolete browsers, or pass in domElements found using other selector such as jQuery sizzle or Zepto.
+     */
+    function minidocs(docs, domEls) {
+      if (typeof domEls !== "object") domEls = document.querySelectorAll(domEls || 'code span.function');
+      // Could also do 'code span.method'
+      var hits = 0, missed = [];
+      for (var i = 0, t, len = domEls && domEls.length; i < len; ++i) {
+        t = docs[domEls[i].textContent];
+        if (null != t) {
+          ++hits;
+          domEls[i].title = t;
+        } else {
+          missed.push(domEls[i].textContent);
+        }
+      }
+      var misses = missed.length;
+      missed = uniq(missed);
+      return {
+        hits: hits,
+        misses: misses,
+        perc: 100 * hits / (hits + misses),
+        missed: missed,
+        toString: asString,
+        show: function(str) { return docs[str]; },
+        clear: function(doc,els) { return clearTitles(doc || docs, els || domEls); }
+      }
+    }
+    /** Remove duplicates if ra.filter and ra.sort are defined. Args: array ra */
+    function uniq(ra) {
+      if (ra.filter) ra = ra.slice().sort().filter(function (cur, n, col) {
+        return col[n - 1] != cur; // unique
+      });
+      return ra;
+    }
+    /** Erase all titles on for elements in docs. See also: minidocs. Args: object docs, optional domEls collection or selector string */
+    function clearTitles(docs, domEls) {
+      var empty = {};
+      for (var key in docs) empty[key] = "";
+      return minidocs(empty, domEls);
+    }
+    /** Convenience function to display returned results as a String. See: minidocs. */
+    function asString() {
+      return Math.round(this.perc) + "% = " + this.hits + " hits : " + this.misses + " misses. Need: " + this.missed;
+    }
+    
+    // Can be used as an AMD module, a Node.js module, or a global.
+    if (typeof exports !== "undefined") {
+        // Server
+        var doc = require('doc').doc;
+        var addDocs = function addDocs(docs,els) { 
+            doc: "Adds documentation which will be shown with help(function). Args: object docs {funcName:doc_string,...}, els as module or prototype or array of functions."
+            if (null == els || typeof els == "string") els = global; 
+            var hits = 0, missed = [];
+            for (var f in els) { 
+                if ("function" == typeof els[f]) {
+                  if (null != docs[f]) {
+                      els[f].doc = docs[f];
+                      ++hits;
+                  } else {
+                      missed.push(f);
+                  }
+                }
+            }
+            var misses = missed.length;
+            missed = uniq(missed);
+            return {
+                hits: hits,
+                misses: misses,
+                perc: 100 * hits / (hits + misses),
+                missed: missed,
+                toString: asString,
+                show: function(str) { return docs[str]; },
+                clear: function(doc,items) { return clearTitles(doc || docs, items || els); }
+            }
+        }; //TODO: combine with dom minidocs using Strategy pattern?
+        if (typeof module !== "undefined" && module.exports) {
+            exports = module.exports = addDocs;
+            exports.docs = exports.help = doc;
+            exports.clear = clearTitles;
+        }
+        minidocs = exports.minidocs = addDocs;
+    } else if (typeof define === "function" && define.amd) {
+        // AMD
+        define(function() {
+            return minidocs;
+        });
+    } else {
+        // Global scope
+        window.minidocs = minidocs;
+    }
+})();
---
+++ web/stapes_docs.js
@@ -0,0 +1,23 @@
+if (typeof require !== "undefined") minidocs = require('./minidocs');
+var docs = {
+  "Stapes": "A (really) tiny Javascript MVC microframework. See also: Stapes.create",
+  "create": "Create a new instance of a Stapes object. See also: mixinEvents, extend, on",
+  "each": "Iterate over all attributes of a module. Args: function, optional context (default: this module).",
+  "emit": "Send event(s) to the module. Args: string eventName(s), optional data.",
+  "extend": "Extend your module by giving an object from which to copy attributes. Args: optional context (default=this module), object.",
+  "filter": "Iterate over all attributes of a module. Args: function, optional context",
+  "get": "Gets an attribute by key. If the item is not available will return null. See also: set, has, getAll. Args: key (string) or filter function.",
+  "getAll": "Returns copy of all the attributes of a module as an object. See also: get, getAllAsArray",
+  "getAllAsArray": "Returns all attributes as an array. See also: get, getAll. Args: (None).",
+  "has": "Checks if a key is available and returns true or false. See also: get, set.",
+  "mixinEvents": "Add Stapes event handlers to existing (or new if blank) object. Args: optional object.",
+  "off": "Removes event handler(s) from this object. Args: optional string eventType, optional function handler.",
+  "on": "Add an event listener triggered by emit. Args: string eventName, function handler, optional context OR object {eventName1: handler1, eventName2: handler2, ...}, optional context.",
+  "push": "Sets a value, automatically generates an unique uuid as a key. Args: array or value, optional boolean silent.",
+  "remove": "Deletes an attibute. Opposite of `.set(key)`. Args: key or function, optional boolean silent.",
+  "set": "Sets an attribute of key to value, triggering an event unless `silent` is true. See also: get, emit. Args: string key, value, optional boolean silent.",
+  "size": "Returns the number of attributes in a module.",
+  "update": "Updates an attribute with a new value, based on the return value of a function. Args: optional key, function."
+}
+minidocs(docs).hits || setTimeout( function() { minidocs(docs); }, 500 );
+if (typeof exports === "object") { exports.minidocs = minidocs, exports.docs = docs }
