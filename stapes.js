78d1163a037d2d8fb26c26bd4b9e0774e1dce893
Hay Kranen <huskyr@gmail.com>
2016-07-05 23:55:31 +0200

    Releasing v1.0.0

--- stapes.js
+++ stapes.js
@@ -16,7 +16,7 @@
 ;(function() {
     'use strict';
 
-    var VERSION = "0.8.1";
+    var VERSION = "1.0.0";
 
     // Global counter for all events in all modules (including mixed in objects)
     var guid = 1;
